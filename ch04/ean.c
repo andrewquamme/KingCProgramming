4ab4ff3037338c6e3fdf4657ac5a60e6e819bb30
Andrew Quamme <andrewquamme@gmail.com>
2020-08-28 16:30:46 -0700

    ch 04 projects complete

--- ch04/ean.c
+++ ch04/ean.c
@@ -5,7 +5,7 @@
 int main(void)
 {
     int i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12;
-    int first_sum, second_sum, total, check;
+    int first_sum, second_sum, total;
 
     printf("Enter the first 12 digits of an EAN: ");
 
@@ -15,9 +15,8 @@ int main(void)
     first_sum = i2 + i4 + i6 + i8 + i10 + i12;
     second_sum = i1 + i3 + i5 + i7 + i9 + i11;
     total = (first_sum * 3) + second_sum;
-    check = 9 - (--total % 10);
-
-    printf("Check digit: %d\n", check);
+  
+    printf("Check digit: %d\n", 9 - (--total % 10) );
 
     return 0;
 }
